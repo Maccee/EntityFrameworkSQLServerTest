312738fa5821e26173386f8c9803cd3da5f2cbd1
Macce <makkeeh@gmail.com>
2023-10-09 10:43:03 +0300

    fix

--- Models/TutorialDbContext.cs
+++ Models/TutorialDbContext.cs
@@ -18,7 +18,7 @@ public partial class TutorialDbContext : DbContext
     public virtual DbSet<Customer> Customers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+
         => optionsBuilder.UseSqlServer("Server=localhost,57000;Database=TutorialDB;User Id=sa;Password=Str#ng_Passw#rd;Trusted_Connection=False;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
